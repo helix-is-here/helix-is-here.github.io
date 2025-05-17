53a88256e2a3aeb237c539b555a7d5eaf1c6de17
Jake Grover <102583574@student.swin.edu.au>
2025-05-17 12:48:13 +1000

    live server with API fetching

--- divisions.js
+++ divisions.js
@@ -1,70 +1,15 @@
-divisions = [
-    {
-    "id": "ee3cafaa-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Championship Men"
-    },
-    {
-    "id": "ee4035f8-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Championship Women"
-    },
-    {
-    "id": "ee43298e-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Division One Men"
-    },
-    {
-    "id": "ee46747c-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Division One Women"
-    },
-    {
-    "id": "ee4a0420-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Division Two Men"
-    },
-    {
-    "id": "ee4d24e8-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Division Two Women"
-    },
-    {
-    "id": "ee501d7e-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Victorian Youth Championship Men"
-    },
-    {
-    "id": "ee53fe30-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Victorian Youth Championship Women"
-    },
-    {
-    "id": "ee57fec2-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Youth League One Men"
-    },
-    {
-    "id": "ee64436c-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Youth League One Women"
-    },
-    {
-    "id": "ee615c24-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Youth League Two Men"
-    },
-    {
-    "id": "ee6709f8-76a3-11eb-a481-2a86bfd2d24d",
-    "name": "Youth League Two Women"
-    },
-    {
-    "id": "a5809a1b-b1d9-11ef-823f-656618bcf3f0",
-    "name": "Youth League Three Men"
-    },
-    // {
-    // "id": "2640caa7-b5dc-11ee-ad85-ab77236954ef",
-    // "name": "Youth Qualification Series - VYCW/YL1W"
-    // },
-    // {
-    // "id": "26391514-b5dc-11ee-8bb3-ab77236954ef",
-    // "name": "Youth Qualification Series - VYCM/YL1M"
-    // },
-    // {
-    // "id": "2647c4f2-b5dc-11ee-85ed-ab77236954ef",
-    // "name": "Youth Qualification Series - YL1M/YL2M"
-    // },
-    // {
-    // "id": "264f12e6-b5dc-11ee-947f-ab77236954ef",
-    // "name": "Youth Qualification Series - YL1W/YL2W"
-    // }
-]
+export const divisions = [
+  { id: "ee3cafaa-76a3-11eb-a481-2a86bfd2d24d", name: "Championship Men" },
+  { id: "ee4035f8-76a3-11eb-a481-2a86bfd2d24d", name: "Championship Women" },
+  { id: "ee43298e-76a3-11eb-a481-2a86bfd2d24d", name: "Division One Men" },
+  { id: "ee46747c-76a3-11eb-a481-2a86bfd2d24d", name: "Division One Women" },
+  { id: "ee4a0420-76a3-11eb-a481-2a86bfd2d24d", name: "Division Two Men" },
+  { id: "ee4d24e8-76a3-11eb-a481-2a86bfd2d24d", name: "Division Two Women" },
+  { id: "ee501d7e-76a3-11eb-a481-2a86bfd2d24d", name: "Victorian Youth Championship Men" },
+  { id: "ee53fe30-76a3-11eb-a481-2a86bfd2d24d", name: "Victorian Youth Championship Women" },
+  { id: "ee57fec2-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League One Men" },
+  { id: "ee64436c-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League One Women" },
+  { id: "ee615c24-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League Two Men" },
+  { id: "ee6709f8-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League Two Women" },
+  { id: "a5809a1b-b1d9-11ef-823f-656618bcf3f0", name: "Youth League Three Men" }
+];
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("hello world...")
+import { divisions } from "./divisions.js";
+
+async function getDivisionSeasonsData(divisionId) {
+  const url = "https://prod.services.nbl.com.au/api_cache/bbv/synergy";
+  const params = new URLSearchParams({
+    route: `competitions/${divisionId}/seasons`,
+    format: "true"
+  });
+
+  try {
+    const response = await fetch(`${url}?${params}`);
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Season data:", data);
+      // You can add logic here to display data in the UI
+    } else {
+      console.error("Request failed:", response.status);
+    }
+  } catch (err) {
+    console.error("Fetch error:", err);
+  }
+}
+
+// Populate the division select element
+const select = document.getElementById("divisionSelect");
+
+divisions.forEach((div) => {
+  const option = document.createElement("option");
+  option.value = div.id;
+  option.textContent = div.name;
+  select.appendChild(option);
+});
+
+// Listen for user selection
+select.addEventListener("change", () => {
+  const selectedId = select.value;
+  if (selectedId) {
+    getDivisionSeasonsData(selectedId);
+  }
+});
