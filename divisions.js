a9e36eec5d39350a479de57684a064dc5e5a2786
Jake Grover <102583574@student.swin.edu.au>
2025-05-24 13:41:48 +1000

    progress

---
+++ divisions.js
@@ -0,0 +1,15 @@
+divisions = [
+    { id: "ee3cafaa-76a3-11eb-a481-2a86bfd2d24d", name: "Championship Men", group: "Senior Men", seasons: []},
+    { id: "ee4035f8-76a3-11eb-a481-2a86bfd2d24d", name: "Championship Women", group: "Senior Women" },
+    { id: "ee43298e-76a3-11eb-a481-2a86bfd2d24d", name: "Division One Men", group: "Senior Men" },
+    { id: "ee46747c-76a3-11eb-a481-2a86bfd2d24d", name: "Division One Women", group: "Senior Men" },
+    { id: "ee4a0420-76a3-11eb-a481-2a86bfd2d24d", name: "Division Two Men", group: "Senior Men" },
+    { id: "ee4d24e8-76a3-11eb-a481-2a86bfd2d24d", name: "Division Two Women", group: "Senior Men" },
+    { id: "ee501d7e-76a3-11eb-a481-2a86bfd2d24d", name: "Victorian Youth Championship Men", group: "Youth Men" },
+    { id: "ee53fe30-76a3-11eb-a481-2a86bfd2d24d", name: "Victorian Youth Championship Women", group: "Youth Women" },
+    { id: "ee57fec2-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League One Men", group: "Youth Men" },
+    { id: "ee64436c-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League One Women", group: "Youth Women" },
+    { id: "ee615c24-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League Two Men", group: "Youth Men" },
+    { id: "ee6709f8-76a3-11eb-a481-2a86bfd2d24d", name: "Youth League Two Women", group: "Youth Women" },
+    { id: "a5809a1b-b1d9-11ef-823f-656618bcf3f0", name: "Youth League Three Men", group: "Youth Men" }
+];
