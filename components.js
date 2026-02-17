3fb81828375ae9e83370c3a33aa5bb47e93fb7d2
Jake Grover <jakeox180@gmail.com>
2026-02-17 11:14:05 +1100

    trying to fix routing

--- components.js
+++ components.js
@@ -1,9 +1,3 @@
-const { createApp } = Vue;
-
-/* =========================
-   Shared Components
-========================= */
-
 const NavbarComponent = {
   template: `
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
