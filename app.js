775fe8c7a6502bd850c97bd29088e258cd28d426
Jake Grover <jakeox180@gmail.com>
2026-07-20 17:49:28 +1000

    updated homepage

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const HomePage = {
   template: `
     <div>
       <h1 class="mb-3">Helix Signals</h1>
-      <p class="lead">Training tools for communications.</p>
-      <p>If you have any suggestions for new features or tools, please feel free to reach out.</p>
+      <p class="lead">Training tools for communicators.</p>
+      <p>If you have any suggestions for bug fixes or new features, please feel free to reach out.</p>
     </div>
   `
 };
